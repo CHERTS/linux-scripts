77d9bfce147a0067f8673857cf412430b686b151
Mikhail Grigorev <sleuthhound@gmail.com>
2017-09-29 11:03:29 +0500

    Added script for updating monit to the latest stable version in Debian-like distributions
    Signed-off-by: Mikhail Grigorev <sleuthhound@gmail.com>

---
+++ monit/README.md
@@ -0,0 +1,7 @@
+# Script for updating monit to the latest stable version in Debian-like distributions
+
+[По-русски / In Russian](README.ru.md)
+
+Script for updating [monit](https://mmonit.com/monit/) to the latest stable version in Debian-like distributions.<br>
+
+Author: Mikhail Grigoryev <sleuthhound@gmail.com>
---
+++ monit/monit-upgrade.sh
@@ -0,0 +1,160 @@
+#!/usr/bin/env bash
+
+#
+# Program: Monit upgrade <monit-upgrade>
+#
+# Author: Mikhail Grigorev < sleuthhound at gmail dot com >
+# 
+# Current Version: 1.0
+#
+# Revision History:
+#
+#  Version 1.0
+#    Initial Release
+#
+
+set -e
+
+monit_latest_ver="5.24.0"
+# linux-x64 or linux-x86
+monit_platform=linux-x64
+monit_url=https://mmonit.com/monit/dist/binary/${monit_latest_ver}/monit-${monit_latest_ver}-${monit_platform}.tar.gz
+monit_tmp_dir=/tmp
+ 
+command_exists ()
+{
+    type "$1" &> /dev/null ;
+}
+ 
+if ! command_exists monit ; then
+    echo "ERROR: monit not installed!"
+    if [ -f /etc/debian_version ]; then
+        DEBIAN_VERSION=$(sed 's/\..*//' /etc/debian_version)
+        if [ ${DEBIAN_VERSION} == '9' ]; then
+            echo "NOTICE: Please, run: apt-get install monit"
+        elif [ ${DEBIAN_VERSION} == '8' ]; then
+            echo "NOTICE: Please, run: apt-get install monit"
+        elif [ ${DEBIAN_VERSION} == '7' ]; then
+            echo "NOTICE: Please, run: apt-get install monit"
+        else
+            echo "ERROR: Unknown Debian version"
+            exit 1;
+        fi
+    else
+        echo "ERROR: Unknown Linux version"
+        exit 1;
+    fi
+    exit 1;
+else
+    monit_bin=`which monit`
+    sed_bin=`which sed`
+fi
+
+current_monit_ver=$(${monit_bin} -V | ${sed_bin} -n 1p | ${sed_bin} 's/[[:alpha:]|(|[:space:]]//g' | awk -F- '{print $1}')
+
+if [ ${current_monit_ver}  == ${monit_latest_ver} ]; then
+    echo "You are using the latest Monit version (v${current_monit_ver})"
+    exit 0;
+fi
+
+if ! command_exists wget ; then
+    echo "ERROR: wget not installed!"
+    exit 1;
+else
+    wget_bin=`which wget`
+fi
+
+if ! command_exists tar ; then
+    echo "ERROR: tar not installed!"
+    exit 1;
+else
+    tar_bin=`which tar`
+fi
+
+if [ ! -f "${monit_tmp_dir}/monit-${monit_latest_ver}-${monit_platform}.tar.gz" ]; then
+    echo -n "Downloading new monit version (v${monit_latest_ver})... "
+    ${wget_bin} ${monit_url} -O "${monit_tmp_dir}/monit-${monit_latest_ver}-${monit_platform}.tar.gz" >/dev/null 2>&1
+    if [ -f "${monit_tmp_dir}/monit-${monit_latest_ver}-${monit_platform}.tar.gz" ]; then
+	    echo "Done"
+    else
+	    echo "Error"
+	    exit 1;
+    fi
+fi
+
+if [ -f "${monit_tmp_dir}/monit-${monit_latest_ver}-${monit_platform}.tar.gz" ]; then
+    if [ -d "${monit_tmp_dir}/monit-${monit_latest_ver}" ]; then
+        rm -rf "${monit_tmp_dir}/monit-${monit_latest_ver}"
+    fi
+    echo -n "Unpacking monit-${monit_latest_ver}-${monit_platform}.tar.gz... "
+    ${tar_bin} -zxf "${monit_tmp_dir}/monit-${monit_latest_ver}-${monit_platform}.tar.gz" -C "${monit_tmp_dir}/"
+    if [ -d "${monit_tmp_dir}/monit-${monit_latest_ver}" ]; then
+       echo "Done"
+    else
+       echo "Error"
+       exit 1;
+    fi
+    case "$(pidof monit | wc -w)" in
+       0)  echo "Monit not running."
+	   ;;
+       1)  echo -n "Stoping current monit... "
+           if [ -f /etc/init.d/monit ]; then
+               /etc/init.d/monit stop >/dev/null 2>&1
+	   else
+	       echo "Error"
+               echo "ERROR: File /etc/init.d/monit not found."
+	       exit 1;
+	   fi
+	   ;;
+       *)  echo "Kill current monit... "
+	   kill -9 $(pidof monit | awk '{print $1}')
+	   ;;
+    esac
+    case "$(pidof monit | wc -w)" in
+       0)  echo "Done"
+	   ;;
+       1)  echo "Error"
+	   echo "ERROR: monit is not stopped."
+	   exit 1;
+    esac
+    echo -n "Delete older version monit... "
+    if [ -f ${monit_bin} ]; then
+            rm -f ${monit_bin}
+    fi
+    if [ ! -f ${monit_bin} ]; then
+	    echo "Done"
+    else
+	    echo "Error"
+    fi
+    echo -n "Copy the new version of monit... "
+    cp "${monit_tmp_dir}/monit-${monit_latest_ver}/bin/monit" ${monit_bin}
+    chown -R root:root ${monit_bin}
+    chmod a+x ${monit_bin}
+    echo "Done"
+    if [ ! -f /etc/monitrc ]; then
+        echo -n "Creatin symlink... "
+        ln -s /etc/monit/monitrc /etc/monitrc
+	echo "Done"
+    fi
+    echo -n "Starting new monit... "
+    if [ -f /etc/init.d/monit ]; then
+        /etc/init.d/monit start >/dev/null 2>&1
+    else
+	echo "Error"
+        echo "ERROR: File /etc/init.d/monit not found"
+        exit 1;
+    fi
+    case "$(pidof monit | wc -w)" in
+	0)  echo "Error"
+            echo "ERROR: monit is not running."
+	    ;;
+        1)  echo "Done"
+	    ;;
+    esac
+    if [ -d "${monit_tmp_dir}/monit-${monit_latest_ver}" ]; then
+        rm -rf "${monit_tmp_dir}/monit-${monit_latest_ver}"
+    fi
+    if [ -f "${monit_tmp_dir}/monit-${monit_latest_ver}-${monit_platform}.tar.gz" ]; then
+	rm "${monit_tmp_dir}/monit-${monit_latest_ver}-${monit_platform}.tar.gz"
+    fi
+fi
