32508e39d6d86b736fa86ff65104548c07e02d17
Mikhail Grigorev <sleuthhound@gmail.com>
2023-01-19 11:05:21 +0200

    Fixed zabbix partitiong procedure (slave check)

--- zabbix/zabbix_db_partitiong/zabbix_partitiong_maintenance_all.sql
+++ zabbix/zabbix_db_partitiong/zabbix_partitiong_maintenance_all.sql
@@ -2,9 +2,10 @@ DELIMITER $$
 DROP PROCEDURE partition_maintenance_all;
 CREATE PROCEDURE `partition_maintenance_all`(SCHEMA_NAME VARCHAR(32))
 BEGIN
-DECLARE SlaveNum Integer;
-SELECT COUNT(1) INTO SlaveNum FROM performance_schema.replication_connection_status;
-IF SlaveNum = 0 THEN
+DECLARE DBStatus Integer;
+--SELECT COUNT(1) INTO DBStatus FROM performance_schema.replication_connection_status;
+SELECT @@global.read_only INTO DBStatus;
+IF DBStatus = 0 THEN
                 CALL partition_maintenance(SCHEMA_NAME, 'history', 30, 24, 3);
                 CALL partition_maintenance(SCHEMA_NAME, 'history_log', 30, 24, 3);
                 CALL partition_maintenance(SCHEMA_NAME, 'history_str', 30, 24, 3);
