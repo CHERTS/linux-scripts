cc5d640ad2997bf6cce1566fccf8d5ed07dd4993
Mikhail Grigorev <sleuthhound@gmail.com>
2024-03-29 12:19:54 +0500

    Update clickhouse-backup.sh

--- clickhouse/clickhouse-backup.sh
+++ clickhouse/clickhouse-backup.sh
@@ -5,9 +5,12 @@
 #
 # Author: Mikhail Grigorev < sleuthhound at gmail dot com >
 #
-# Current Version: 1.0
+# Current Version: 1.1
 #
 # Revision History:
+#
+#  Version 1.1
+#    Added caclulate duration of executing script
 #
 #  Version 1.0
 #    Initial Release
@@ -40,18 +43,16 @@ if [ -f "${SCRIPT_DIR}/${SCRIPT_NAME%.*}.conf" ]; then
     source "${SCRIPT_DIR}/${SCRIPT_NAME%.*}.conf"
 fi
 
-# trap ctrl-c and call ctrl_c()
-trap ctrl_c INT
-
-function ctrl_c() {
-        echo "** Trapped CTRL-C"
-        exit 1
-}
-
 _command_exists() {
         type "$1" &> /dev/null
 }
 
+# Don't edit this config
+LOG_FILE_WRITABLE=1
+LOG_FILE_EXIST_WRITABLE=1
+DATE_START=$(date +"%s")
+
+# Logging function
 _logging() {
         local MSG=${1}
         local ENDLINE=${2:-"1"}
@@ -60,31 +61,71 @@ _logging() {
         else
                 printf "%s: %s\n" "$(date "+%d.%m.%Y %H:%M:%S")" "${MSG}" 2>/dev/null
         fi
-        printf "%s | %s: %s\n" "$(date "+%d.%m.%Y %H:%M:%S")" "$$" "${MSG}" 1>>"${LOG_FILE}" 2>&1
+        if [ ! -f "${LOG_FILE}" ] && [[ "${LOG_FILE_WRITABLE}" -eq 1 ]]; then
+                touch "${LOG_FILE}" >/dev/null 2>&1
+                if [ $? -ne 0 ]; then
+                        printf "%s: %s\n" "$(date "+%d.%m.%Y %H:%M:%S")" "WARNING: Log file '${LOG_FILE}' is not writable." 2>/dev/null
+                        LOG_FILE_WRITABLE=0
+                fi
+        fi
+        if [ -w "${LOG_FILE}" ]; then
+                printf "%s | %s: %s\n" "$(date "+%d.%m.%Y %H:%M:%S")" "$$" "${MSG}" 1>>"${LOG_FILE}" 2>&1
+        else
+                if [[ "${LOG_FILE_EXIST_WRITABLE}" -eq 1 ]] && [[ "${LOG_FILE_WRITABLE}" -eq 1 ]]; then
+                        printf "%s: %s\n" "$(date "+%d.%m.%Y %H:%M:%S")" "WARNING: Log file '${LOG_FILE}' is not writable." 2>/dev/null
+                        LOG_FILE_EXIST_WRITABLE=0
+                fi
+        fi
 }
 
+# Calculate duration function
+_duration() {
+        local DATE_START=${1:-"$(date +'%s')"}
+        local FUNC_NAME=${2:-""}
+        local DATE_END=$(date +"%s")
+        local D_MSG=""
+        local DATE_DIFF=$((${DATE_END}-${DATE_START}))
+        if [ -n "${FUNC_NAME}" ]; then
+            local D_MSG=" of execute function '${FUNC_NAME}'"
+        fi
+        _logging "Duration${D_MSG}: $((${DATE_DIFF} / 3600 )) hours $(((${DATE_DIFF} % 3600) / 60)) minutes $((${DATE_DIFF} % 60)) seconds"
+}
+
+# Fail, log and exit script function
 _fail() {
         local MSG=${1}
         _logging "${MSG}"
+        _duration "${DATE_START}"
         _logging "End script '${SCRIPT_DIR}/${SCRIPT_NAME}'. Goodbye ;)"
         exit 1
 }
 
+# trap ctrl-c and call ctrl_c()
+trap ctrl_c INT
+
+function ctrl_c() {
+        _fail "** Trapped CTRL-C"
+        exit 1
+}
+
+_logging "Starting script '${SCRIPT_DIR}/${SCRIPT_NAME}'"
+
 if _command_exists "clickhouse-backup"; then
 	CHBACKUP_BIN=$(which clickhouse-backup)
 else
-        echo "ERROR: Command 'clickhouse-backup' not found, please download and install latest version from https://github.com/AlexAkulov/clickhouse-backup"
-        exit 1
+        _fail "ERROR: Command 'clickhouse-backup' not found, please download and install latest version from https://github.com/AlexAkulov/clickhouse-backup"
 fi
 
 if _command_exists "rsync"; then
         RSYNC_BIN=$(which rsync)
 else
-        echo "ERROR: Command 'rsync' not found."
-        exit 1
+        _fail "ERROR: Command 'rsync' not found."
 fi
 
-_logging "Starting script '${SCRIPT_DIR}/${SCRIPT_NAME}'"
+_logging "Current script configuration:"
+_logging "LOG_FILE: ${LOG_FILE}"
+_logging "CLICKHOUSE_BASE_DIR: ${CLICKHOUSE_BASE_DIR}"
+_logging "RSYNC_BACKUP_STORAGE: ${RSYNC_BACKUP_STORAGE}"
 
 _logging "Run clickhouse-backup..."
 ${CHBACKUP_BIN} create ${CLICKHOUSE_BACKUP_NAME} ${CLICKHOUSE_BACKUP_OPTS} 1>>"${LOG_FILE}" 2>&1
@@ -122,5 +163,7 @@ if [ -d "${RSYNC_BACKUP_STORAGE}" ]; then
 	fi
 fi
 
-_logging "End script '${SCRIPT_DIR}/${SCRIPT_NAME}'. Goodbye ;)"
+_logging "All done."
+_duration "${DATE_START}"
 
+_logging "End script '${SCRIPT_DIR}/${SCRIPT_NAME}'. Goodbye ;)"
