95d4692a156f96f431a3c8ea1d12ad4bcc6e5afc
Mikhail Grigorev <sleuthhound@gmail.com>
2025-06-26 12:21:58 +0500

    Update install_pg_profile.sh

--- postgres/install_pg_profile.sh
+++ postgres/install_pg_profile.sh
@@ -5,7 +5,7 @@
 #
 # Author: Mikhail Grigorev <sleuthhound at gmail dot com>
 # 
-# Current Version: 1.0.2
+# Current Version: 1.0.3
 #
 # License:
 #  This program is distributed in the hope that it will be useful,
@@ -24,9 +24,9 @@ SCRIPT_NAME=$(basename $0)
 
 # Settings
 ENABLE_DEBUG=0
-PG_STAT_KCACHE_GIT_VER=REL2_2_3
-PG_WAIT_SAMPLING_VER=v1.1.5
-PG_PROFILE_GIT_VER=4.5
+PG_STAT_KCACHE_GIT_VER=REL2_3_0
+PG_WAIT_SAMPLING_VER=v1.1.8
+PG_PROFILE_GIT_VER=4.9
 PG_CONGIG_PATH=""
 
 _command_exists() {
@@ -124,7 +124,7 @@ _detect_linux_distrib() {
 			OS_DISTRIB="Debian"
 			echo -en "${GREEN}${DIST} ${REV}"
 			case "${REV}" in
-			14.04|16.04|18.04|20.04|22.04)
+			18.04|20.04|22.04|24.04)
 				echo -e " (${PSUEDONAME})${NC}"
 				;;
 			*)
